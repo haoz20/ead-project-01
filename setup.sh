bcfc410d199ec559b9dd5c9e68cde4c45de0b906
haoz20 <zhaonianhao2005@gmail.com>
2025-12-31 11:19:02 +0700

    Remove exit command from setup.sh and add comment for mu-plugin in docker-compose.yml

--- setup.sh
+++ setup.sh
@@ -23,6 +23,3 @@ sudo mkdir -p /usr/local/lib/docker/cli-plugins
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-linux-x86_64 \
   -o /usr/local/lib/docker/cli-plugins/docker-compose
 sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-
-# exit to apply docker changes
-exit
